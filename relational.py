b5a814f33d2ddafc1c7e070a4663190f71758ee0
solstxce <48374053+solstxce@users.noreply.github.com>
2023-02-16 08:37:09 +0530

    Create relational.py

---
+++ relational.py
@@ -0,0 +1,16 @@
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+operator=input("Enter operator: ")
+if operator=="==":
+    result=(a==b)
+elif operator=="!=":
+    result=(a!=b)
+elif operator=="<":
+    result=(a<b)
+elif operator==">":
+    result=(a>b)
+elif operator=="<=":
+    result=(a<=b)
+elif operator==">=":
+    result=(a>=b)
+print(f"{a} {operator} {b} is {result}.")
