ef6d9306bc94b113c90a0aff62a0bc70c5138f6d
solstxce <48374053+solstxce@users.noreply.github.com>
2023-02-16 08:40:02 +0530

    Create logical.py

---
+++ logical.py
@@ -0,0 +1,11 @@
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+operator=input("Enter operator: ")
+if operator=="and":
+    result=(a<b and a<b*2)
+elif operator=="or":
+    result=(a<b or a<b*2)
+if operator=="not":
+    result=not(a<b and a<b*2)
+print(f"The test case is 'a < b {operator} a < b*2' ")
+print(f"The result is {result}.")
